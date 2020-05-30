e018eb93186a69311de434b32df309fa4430655e
RajeshKumarManivannan <Rajeshkumar.Manivannan@cognizant.com>
2020-05-30 15:08:59 -0400

    Chapter 11 final

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -13,12 +13,12 @@ import org.springframework.web.client.RestTemplate;
 public class RestConfig {
 
     @Bean
-    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
 
     @Bean
+    @LoadBalanced
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
